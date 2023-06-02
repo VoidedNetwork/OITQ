5e2c356ac461f4b267ec0ab4c8a010bab1712160
J4C0B3Y <J4C0B3Y@gmail.com>
2023-06-02 12:21:41 +1000

    Fix stupidity

--- src/main/java/gg/voided/oitq/listeners/ArrowHitListener.java
+++ src/main/java/gg/voided/oitq/listeners/ArrowHitListener.java
@@ -2,6 +2,7 @@ package gg.voided.oitq.listeners;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,12 +14,18 @@ public class ArrowHitListener implements Listener {
     @EventHandler
     public void onArrowHit(EntityDamageByEntityEvent event) {
         if(!(event.getDamager() instanceof Arrow)) return;
-        if(!event.getDamager().hasMetadata("oitq")) return;
+        if(!(event.getEntity() instanceof Player)) return;
+
+        Arrow arrow = (Arrow) event.getDamager();
+
+        if(!arrow.hasMetadata("oitq")) return;
+        if(!(arrow.getShooter() instanceof Player)) return;
 
         Player player = (Player) event.getEntity();
+        Player shooter = (Player) arrow.getShooter();
 
         player.setHealth(0);
 
-        player.getInventory().setItem(8, new ItemStack(Material.ARROW));
+        shooter.getInventory().setItem(8, new ItemStack(Material.ARROW));
     }
 }
